edb18938ff38949572aca7b99b2251979a7aaeb0
adeeshaKSoftcodeit <adeeshaKSoftcodeit>
2024-04-15 13:22:27 +0530

    Initial

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'ecom_db',
+      port: 3306,
+      username: 'root',
+      password: 'qwerty123',
+      entities: [],
+      host: 'localhost',
+      synchronize: true,
+    }),
+    UsersModule,
+    ProductsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
