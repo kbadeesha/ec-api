efe720bebe146a23d1fabfff347e548a0fb1827d
adeeshaKSoftcodeit <adeeshaKSoftcodeit>
2024-09-06 22:44:18 +0530

    Auth Middleware

--- src/users/middleware/current-user.middleware.ts
+++ src/users/middleware/current-user.middleware.ts
@@ -1,7 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
@@ -10,9 +19,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
-
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-expect-error
       req.currentUser = user;
     }
     next();
